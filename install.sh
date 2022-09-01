f5af2452ecc90be15758a2a7dc2d15b67a9d4fba
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-09-01 20:31:48 +0200

    Updated install script

--- install.sh
+++ install.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
 # https://github.com/dubniczky/Shell-Utilities
-# Utility install script
+# Utility to install scripts to binaries
 
 
-install_target="/usr/local/bin"
+INSTALL_TARGET="/usr/local/bin"
 
 # Make scripts user runnable
 chmod +x scripts/*
 
 # Install scripts
-#cp -f scripts/* $install_target
 cd scripts
 for file in *; do
     base_name=$(echo $file | cut -d "." -f 1)
-    cp -f $file "$install_target/$base_name"
+    cp -f $file "$INSTALL_TARGET/$base_name"
 done
 cd ..
 
 # Displaty
-echo "Installed $(ls scripts | wc -l | xargs echo) scripts to $install_target"
+echo "Installed $(ls scripts | wc -l | xargs echo) scripts to $INSTALL_TARGET"
