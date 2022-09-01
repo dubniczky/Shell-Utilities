ed46c6414bac01132e83e07db3e7394e0a3bdbba
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-09-01 20:10:46 +0200

    Added .sh extension to scripts for better IDE support

--- install.sh
+++ install.sh
@@ -4,13 +4,19 @@
 # Utility install script
 
 
-install_target="/usr/local/bin/"
+install_target="/usr/local/bin"
 
 # Make scripts user runnable
 chmod +x scripts/*
 
 # Install scripts
-cp -f scripts/* $install_target
+#cp -f scripts/* $install_target
+cd scripts
+for file in *; do
+    base_name=$(echo $file | cut -d "." -f 1)
+    cp -f $file "$install_target/$base_name"
+done
+cd ..
 
 # Displaty
 echo "Installed $(ls scripts | wc -l | xargs echo) scripts to $install_target"
