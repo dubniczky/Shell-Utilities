74e13e912acea8cfcebbe8473ae933ae186086f4
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-11-26 21:18:22 +0100

    Added location parameter to installer script

--- install.sh
+++ install.sh
@@ -4,7 +4,10 @@
 # Utility to install scripts to binaries
 
 
-INSTALL_TARGET="/usr/local/bin"
+default_target="/usr/local/bin"
+
+install_target=${1:-$default_target}
+install_target=$(realpath $install_target)
 
 # Make scripts user runnable
 chmod +x scripts/*
@@ -13,9 +16,9 @@ chmod +x scripts/*
 cd scripts
 for file in *; do
     base_name=$(echo $file | cut -d "." -f 1)
-    cp -f $file "$INSTALL_TARGET/$base_name"
+    cp -f $file "$install_target/$base_name"
 done
 cd ..
 
 # Displaty
-echo "Installed $(ls scripts | wc -l | xargs echo) scripts to $INSTALL_TARGET"
+echo "Installed $(ls scripts | wc -l | xargs echo) scripts to $install_target"
